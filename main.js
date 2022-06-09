a82a9127ea287afc520c3e663a6ea9eae98c32d1
unicorngirl2010 <86110608+unicorngirl2010@users.noreply.github.com>
2022-06-09 14:01:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,47 @@
-Webcam.set({
-    width: 350,
-    height: 300,
-    image_format: 'png',
-    png_quality: 90
-});
-
-camera = document.getElementById("camer");
-
-Webcam.attach('#camera');
-
-function take_snapshot() {
-    Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'
-    })
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/H1sMCLKXF/model.json', modelReady);
 }
-console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/g_PrpDI11/model.json', modelLoaded);
-
-function modeLoaded() {
-    console.log('Model Loaded!');
+function modelReady() {
+    classifier.classify(gotResults);
 }
 
-function check() {
-    img = document.getElementById('captured_image');
-    classifier.classify(img, gotResult);
-}
+var parrot = 0;
+var cat = 0;
+var dog = 0;
+var tiger = 0;
 
-function gotResult(error, results) {
+function gotResults(error, results) {
     if (error) {
         console.error(error);
-    } else {
+    }
+    else {
         console.log(results);
-        document.getElementById("result_object_name").innerHTML = result[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.tofixed(3) * 100 + "%";
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'Detected voice is of -' + results[0].label;
+        document.getElementById("result_count").innerHTML = 'Detected Dog - ' + dog + ' Detected Cat - ' + cat + ' Detected Tiger - ' + tiger + ' Detected Parrot - ' + parrot;
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + ", " + random_number_g + ", " + random_number_b + ")";
+        document.getElementById("result_count").style.color = "rgb(" + random_number_r + ", " + random_number_g + ", " + random_number_b + ")";
+
+        img = document.getElementById('animal_image');
+
+        if (results[0].label == "dog") {
+            img.src = 'dog.gif';
+            dog = dog + 1;
+        } else if (results[0].label == "cat") {
+            img.src = 'cat.gif';
+            cat = cat + 1;
+        } else if (results[0].label == "parrot") {
+            img.src = 'parrot.gif';
+            parrot = parrot + 1;
+        } else if (results[0].label == "tiger") {
+            img.src = 'tiger.gif';
+            tiger = tiger + 1;
+        } else {
+            img.src = 'listen.gif';
+        }
     }
 }
